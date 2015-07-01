4e9c9fcab4640fcc323ec0a1800d4aa7695b6f37
Jayro Salgado <jsalgado@nearsoft.com>
2015-07-01 11:32:15 -0700

    first steps

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.set('port',(process.env.PORT || 5000));
+
+app.use(express.static(__dirname+'/public'));
+
+app.set('views', __dirname + '/views');
+
+app.get('/', function(request, response) {
+  response.json({data:"yo did it broh!"})
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
